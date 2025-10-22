25388521738ba03eb61d877498c8ae9d09df97ba
jekoja <edachejohnekoja@gmail.com>
2025-10-22 22:47:11 +0100

    file

---
+++ IsNegative/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func IsNegative(n int) {
+	if n <= 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	IsNegative(-3)
+	IsNegative(-2)
+}
